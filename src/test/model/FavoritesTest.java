7bea0bc80207ddfb43036d303dd7f727577d0aae
tnguye55 <tnguye55@student.ubc.ca>
2024-10-06 17:45:33 -0700

    Added testGetLocationByName() in FavoritesTest

--- src/test/model/FavoritesTest.java
+++ src/test/model/FavoritesTest.java
@@ -11,34 +11,39 @@ public class FavoritesTest {
     private Location location1;
     private Location location2;
     private Country testCountry;
+
     @BeforeEach
-    void setUp(){
+    void setUp() {
         favoriteList = new Favorites();
         testCountry = new Country("USA");
         location1 = new Location("Hawaii", testCountry, 5);
         location2 = new Location("New York", testCountry, 4);
     }
+
     @Test
-    void testConstructor(){
+    void testConstructor() {
         assertEquals(0, favoriteList.getFavorites().size());
     }
+
     @Test
-    void testAddOneLocation(){
+    void testAddOneLocation() {
         favoriteList.addLocation(location1);
         assertTrue(favoriteList.contains(location1));
         assertEquals(1, favoriteList.getFavorites().size());
 
     }
+
     @Test
-    void testAddMultipleLocations(){
+    void testAddMultipleLocations() {
         favoriteList.addLocation(location1);
         favoriteList.addLocation(location2);
         assertTrue(favoriteList.contains(location1));
         assertTrue(favoriteList.contains(location2));
         assertEquals(2, favoriteList.getFavorites().size());
     }
+
     @Test
-    void testRemoveOneLocation(){
+    void testRemoveOneLocation() {
         favoriteList.addLocation(location1);
         favoriteList.addLocation(location2);
         favoriteList.removeLocation(location1);
@@ -46,8 +51,9 @@ public class FavoritesTest {
         assertTrue(favoriteList.contains(location2));
         assertEquals(1, favoriteList.getFavorites().size());
     }
+
     @Test
-    void testRemoveMultipleLocations(){
+    void testRemoveMultipleLocations() {
         favoriteList.addLocation(location1);
         favoriteList.addLocation(location2);
         favoriteList.removeLocation(location1);
@@ -56,19 +62,31 @@ public class FavoritesTest {
         assertFalse(favoriteList.contains(location2));
         assertEquals(0, favoriteList.getFavorites().size());
     }
+
     @Test
     void testRemoveNonExistentLocation() {
-    favoriteList.addLocation(location1);
-    favoriteList.removeLocation(location2);
-    assertTrue(favoriteList.contains(location1));
-    assertEquals(1, favoriteList.getFavorites().size());
+        favoriteList.addLocation(location1);
+        favoriteList.removeLocation(location2);
+        assertTrue(favoriteList.contains(location1));
+        assertEquals(1, favoriteList.getFavorites().size());
     }
+
     @Test
     void testAddDuplicateLocation() {
-    favoriteList.addLocation(location1);
-    favoriteList.addLocation(location1);
-    assertEquals(1, favoriteList.getFavorites().size());
-}
+        favoriteList.addLocation(location1);
+        favoriteList.addLocation(location1);
+        assertEquals(1, favoriteList.getFavorites().size());
+    }
+
+    @Test
+    void testGetLocationByName() {
+        favoriteList.addLocation(location1);
+        favoriteList.addLocation(location2);
+        assertEquals(location1, favoriteList.getFavoriteByName("Hawaii"));
+        assertEquals(null, favoriteList.getFavoriteByName("Washington DC"));
+
+
+    }
 
 }
 
