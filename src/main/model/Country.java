fb4effed426530256a5c3f6020cb5794d24a73cd
tnguye55 <tnguye55@student.ubc.ca>
2024-10-06 17:01:23 -0700

    Added Country class implementation

--- src/main/model/Country.java
+++ src/main/model/Country.java
@@ -2,13 +2,14 @@ package model;
 
 // This class represents a country in the system
 public class Country {
+    private String name;
     //REQUIRES: Country's name cannot be empty or null
     //EFFECTS: Construct a country of set name
     public Country(String name){
-
+        this.name = name;
     }
     //EFFECTS: Returns a country's name
     public String getName(){
-        return "";
+        return name;
     }
 }
